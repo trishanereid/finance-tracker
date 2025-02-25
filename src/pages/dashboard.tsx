750e891c8b6af8ab1c221d39b466260971f36043
Trishane Reid <trishanereid17@gmail.com>
2025-02-26 04:47:47 +0530

    create dashboard

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,50 @@
+import { AppSidebar } from "@/components/app-sidebar";
+import { CustomPieChart } from "@/components/pie-chart";
+import TransactionTable from "@/components/transaction-table";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+} from "@/components/ui/breadcrumb";
+import { Separator } from "@/components/ui/separator";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+
+
+export default function Dashboard() {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbLink href="#">Dashboard</BreadcrumbLink>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+            <CustomPieChart />
+            <CustomPieChart />
+            <CustomPieChart />
+          </div>
+          <div className="grid auto-rows-min gap-4 md:grid-cols-2">
+            <TransactionTable />
+            <TransactionTable />
+          </div>
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
